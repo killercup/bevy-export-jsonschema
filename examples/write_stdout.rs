bb950bc211cf9caa00f029fd47d800231677e251
Pascal Hertleif <pascal@technocreatives.com>
2023-12-15 12:51:25 +0100

    include bevy_xpbd_3d in example for more types to test

--- examples/write_stdout.rs
+++ examples/write_stdout.rs
@@ -1,8 +1,10 @@
 use bevy::prelude::*;
 use bevy_export_jsonschema::ExportTypesExt;
+use bevy_xpbd_3d::plugins::PhysicsPlugins;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
+    app.add_plugins(PhysicsPlugins::default());
     app.export_types(std::io::stdout());
 }
